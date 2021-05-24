2fefc8f231d24517bc400c556fc6d257892107c2
c108112168 <76207012+c108112168@users.noreply.github.com>
2021-05-25 02:51:26 +0800

    Add files via upload

--- 程式碼/optic flow.py
+++ 程式碼/optic flow.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
-
-cap = cv2.VideoCapture('slow.avi')
+import matplotlib.pyplot as plt
+cap = cv2.VideoCapture('slow_2.avi')
 
 
 # 角點設定參數
-feature_params = dict(maxCorners=200,#數量(優先找最強的角點)
-                      qualityLevel=0.001,#去掉(最好等級*數字)以下的角點
-                      minDistance=50,#角之間的最小距離(不能太近)
+feature_params = dict(maxCorners=1,#數量(優先找最強的角點)
+                      qualityLevel=0.1,#去掉(最好等級*數字)以下的角點
+                      minDistance=10,#角之間的最小距離(不能太近)
                       blockSize=10)#
 
 # lucas kanade(光流)設定參數
@@ -16,7 +16,7 @@ lk_params = dict(winSize=(30, 30),#每個位置的搜尋範圍大小
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))#終止條件
 
 # 隨機顏色產生
-color = np.random.randint(0, 255, (100, 3))
+color = np.random.randint(0, 255, (200, 3))
 
 # 找出角點
 ret, old_frame = cap.read()#影片每幀讀取?
@@ -43,14 +43,39 @@ while (1):#循環
     good_old = p0[st == 1]
     p0 = p1[st == 1]
     # draw the tracks
+    new_a = 0
+    new_b = 0
+    dy = [0] *1
     for i, (new, old) in enumerate(zip(good_new, good_old)):
+        #old_a = new_a
+        old_b = new_b
         a, b = new.ravel()
         c, d = old.ravel()
         mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
         frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+        #print (a, b)
+        #new_a = a
+        new_b = b
+        #dx = old_a - new_a
+        dy[i] = old_b - new_b
+        print(dy[i])
+        print(i)
+
+
     img = cv2.add(frame, mask)
 
-    cv2.imshow('frame', img)
+    # 中位
+    maxdy = np.mean(dy)
+    print(maxdy)
+    #exit()
+    # 向量計算
+    rows, cols, xx = img.shape
+    # 平移矩阵M：[[1,0,x],[0,1,y]]
+    M = np.float32([[1, 0, 0], [0, 1, maxdy]])
+    dst = cv2.warpAffine(img, M, (cols, rows))
+    #exit()
+
+    cv2.imshow('frame', dst)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
@@ -58,6 +83,11 @@ while (1):#循環
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1, 1, 2)
+
+
+
+
+
 
 
 cap.release()
